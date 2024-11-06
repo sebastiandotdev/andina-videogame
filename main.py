8313d89cc29ae9504ff670bf96a0f224f701f0f5
sebastiandotdev <johancs.mm@gmail.com>
2024-11-06 18:47:00 -0500

    Add render screen

--- main.py
+++ main.py
@@ -1,6 +1,52 @@
-def main():
-    print("Hello from andina-videogame!")
+import pygame
+from os.path import join
+
+pygame.init()
+pygame.display.set_caption("Platformer")
+
+WIDTH = 1000
+HEIGHT = 800
+FSP = 60
+PLAYER_SPEED = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+def get_background(name: str):
+    image = pygame.image.load(join("assets", name))
+    _, _, width, height = image.get_rect()
+
+    tiles = []
+
+    for x in range(WIDTH // width + 1):
+        for y in range(HEIGHT // height + 1):
+            pos = (x * width, y * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
+
+def main(window):
+    background, bg_image = get_background("blue.png")
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        draw(window, background, bg_image)
+
+    pygame.quit()
+    quit()
 
 
 if __name__ == "__main__":
-    main()
+    main(window)
