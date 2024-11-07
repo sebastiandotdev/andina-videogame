935c51fa2bb1ede57e9beddc48b7f36fc0eeafd5
sebastiandotdev <johancs.mm@gmail.com>
2024-11-06 19:16:45 -0500

    Upload main character

---
+++ player.py
@@ -0,0 +1,9 @@
+import pygame
+
+"""
+**Sprite**:
+Sprite es una clase que representa un objeto en la ventana del juego.
+Puede ser un objeto de juego, un personaje, un enemigo, etc.
+"""
+class Player(pygame.sprite.Sprite):
+    pass
